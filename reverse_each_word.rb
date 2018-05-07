d64a2d75bb0220c55a3ffb1a52e7f239663b1a0a
Elana Ryznar <eryzna@gmail.com>
2018-05-07 00:02:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ def reverse_each_word(word)
   revwordarray=[]
   array.each {|revword| revwordarray << revword.reverse!}
   revwordarray.join (" ")
-  array.collect do |revword|
+  array.collect do |revword|.reverse!
   end
 end
